08e24191dbd49638ec5c886af1111b51a6c33396
Sarad Rajbhandari <saradraj@Sarads-Air.lan>
2024-03-19 22:24:06 -0500

    Add bean validation closes #3

--- src/main/java/guru/springframework/msscbeerservice/web/model/BeerStyleEnum.java
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerStyleEnum.java
@@ -1,14 +1,5 @@
 package guru.springframework.msscbeerservice.web.model;
 
 public enum BeerStyleEnum {
-    LAGER,
-    PILSNER,
-    STOUT,
-    GOSE,
-    PORTER,
-    ALE,
-    WHEAT,
-    IPA,
-    PALE_ALE,
-    SAISON
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
 }
